409075bedf5b602bba4755af32de1ac80d4a808f
Oleksandr Kropivnyi <kropivnyy@gmail.com>
2025-05-30 21:06:23 +0300

    custom cms init

--- src/views/AdminCMSPage.vue
+++ src/views/AdminCMSPage.vue
@@ -3,3 +3,16 @@
     <iframe src="/admin/index.html" class="w-full h-full border-none" style="height: 100%; width: 100%" />
   </div>
 </template>
+
+<script setup>
+// import CMS from 'decap-cms-app';
+// // import YAML from 'yaml';
+// import { onMounted } from 'vue';
+
+// onMounted(() => {
+//   CMS.init();
+//   // fetch('https://hansen-fe.netlify.app/cms/config.yml')
+//   //   .then((res) => res.text())
+//   //   .then((yml) => CMS.init({ config: YAML.parse(yml) }));
+// });
+</script>
