4490e815d6e2d9593adf2dcf85ef04dd6f81f641
Oleksandr Kropivnyi <kropivnyy@gmail.com>
2025-05-31 14:59:38 +0300

    decap init in component

--- src/views/AdminCMSPage.vue
+++ src/views/AdminCMSPage.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="h-screen">
-    <iframe src="/admin/index.html" class="w-full h-full border-none" style="height: 100%; width: 100%" />
+    <div id="nc-root"></div>
+    <!-- <iframe ref="iframeRef" src="/admin/index.html" class="w-full h-full border-none" style="height: 100%; width: 100%" /> -->
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
 import CMS from 'decap-cms-app';
 import YAML from 'yaml';
-import { onMounted } from 'vue';
+
+// const iframeRef = ref(null);
 
 onMounted(() => {
-  // CMS.init();
+  // iframeRef.value.contentWindow.CMS = CMS;
+  // iframeRef.value.contentWindow.QWE = { qwe: 'asd' };
   fetch('https://hansen-fe.netlify.app/cms/config.yml')
     .then((res) => res.text())
-    .then((yml) => CMS.init({ config: YAML.parse(yml) }));
+    .then((yml) => {
+      // const parsed = jsyaml.load(yml);
+      const parsed = YAML.parse(yml);
+      CMS.init({ config: parsed });
+      console.log(CMS);
+    });
 });
 </script>
